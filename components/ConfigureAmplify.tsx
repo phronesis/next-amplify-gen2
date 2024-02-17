912b67d2819686525c5c7337015791a2616fc68a
David Umoh <daveumoh@amazon.com>
2024-02-17 13:22:01 +0100

    add authentication

---
+++ components/ConfigureAmplify.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import config from "@/amplifyconfiguration.json";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure(config), {ssr:true};
+
+export default function ConfigureAmplifyClientSide() {
+    return null;  // this component is only used to configure Amplify on the client-side. It does not render anything. It is only used to configure Amplify on the client-side. It does not render anything. It is only used to configure Amplify on the client-side. It does not render anything. It is only used to configure Amplify on the client-side. It does not render anything. It is only used to configure Amplify on the client-side. It does not render anything. It is only used to configure Amplify on the client-side. It does not render anything. It is only used to configure Amplify on the client-side. It does not render anything. It is only used to configure Amplify on the client-side. It does not render anything. It is only used to configure Amplify on the client-side. It does not render anything. It is only used to configure Amplify on the client-side. It does not render anything. It is only used to configure Amplify on the client-side. It does not render anything. It is only used to configure Amplify on the client-side. It does not render anything. It is only used to configure Amplify
+}
